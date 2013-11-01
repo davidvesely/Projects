5e9debff1d5529cd188616b0f7b24384dd4d3f06
David Vesely <davidvesely@gmail.com>
2013-11-01 20:30:59 +0200

    Homeworks

---
+++ CSharp Part1/02. PrimitiveDataTypes/08.QuotedStrings/QuotedStrings.cs
@@ -0,0 +1,22 @@
+﻿/* 
+ * Declare two string variables and assign them with following value:
+ * 'The "use" of quotations causes difficulties.'
+ * Do the above in two different ways: with and without using quoted strings.
+ */
+
+namespace PrimitiveDataTypes
+{
+    using System;
+
+    class QuotedStrings
+    {
+        static void Main()
+        {
+            string sentence1 = @"The ""use"" of quotations causes difficulties.";
+            string sentence2 = "The \"use\" of quotations causes difficulties.";
+
+            Console.WriteLine(sentence1);
+            Console.WriteLine(sentence2);
+        }
+    }
+}
