102171ee31824c8fd51a1e0d90e996237d6c5b52
David Vesely <davidvesely@gmail.com>
2013-11-01 21:07:22 +0200

    Homeworks

---
+++ CSharp Part1/02. PrimitiveDataTypes/11.ExchangeVariables/ExchangeVariables.cs
@@ -0,0 +1,25 @@
+﻿/* 
+ * Declare  two integer variables and assign them
+ * with 5 and 10 and after that exchange their values.
+ */
+
+namespace PrimitiveDataTypes
+{
+    using System;
+
+    class ExchangeVariables
+    {
+        static void Main()
+        {
+            int firstValue = 5, secondValue = 10;
+            int tempValue;
+            Console.WriteLine("{0} {1}", firstValue, secondValue);
+
+            // Exchange their values with a temporary variable
+            tempValue = firstValue;
+            firstValue = secondValue;
+            secondValue = tempValue;
+            Console.WriteLine("{0} {1}", firstValue, secondValue);
+        }
+    }
+}
