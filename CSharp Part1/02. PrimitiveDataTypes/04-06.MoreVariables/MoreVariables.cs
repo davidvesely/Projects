4ac5bf733efc748d95f2f92268ccd2d65311d7eb
David Vesely <davidvesely@gmail.com>
2013-11-01 08:24:20 +0200

    More Variables task

--- CSharp Part1/02. PrimitiveDataTypes/04-06.MoreVariables/MoreVariables.cs
+++ CSharp Part1/02. PrimitiveDataTypes/04-06.MoreVariables/MoreVariables.cs
@@ -1,5 +1,10 @@
 ﻿/* 
- * 
+ * 4. Declare an integer variable and assign it with the value 254 in hexadecimal format.
+ *    Use Windows Calculator to find its hexadecimal representation.
+ * 5. Declare a character variable and assign it with the symbol that hasUnicode code 72.
+ *    Hint: first use the Windows Calculator to find the hexadecimal representation of 72.
+ * 6. Declare a boolean variable called isFemale and assign
+ *    an appropriate value corresponding to your gender.
  */
 
 namespace PrimitiveDataTypes
@@ -10,7 +15,11 @@ namespace PrimitiveDataTypes
     {
         static void Main()
         {
-            
+            int hexValue = 0xFE;
+            char character = '\u0048'; // 'H' character
+            bool isFemale = false;
+
+            Console.WriteLine("{0} {1} {2}", hexValue, character, isFemale);
         }
     }
 }
