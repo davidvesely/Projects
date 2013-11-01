bffca285a2ba5656dbd9c572ad5e95e23a8ce8e9
David Vesely <davidvesely@gmail.com>
2013-11-01 08:31:23 +0200

    Task 7 - string concat

---
+++ CSharp Part1/02. PrimitiveDataTypes/07.StringConcatenation/StringConcatenation.cs
@@ -0,0 +1,25 @@
+﻿/* 
+ * Declare two string variables and assign them with “Hello” and
+ * “World”. Declare an object variable and assign it with the
+ * concatenation of the first two variables (mind adding an interval).
+ * Declare a third string variable and initialize it with the value of
+ * the object variable (you should perform type casting).
+ */
+
+namespace PrimitiveDataTypes
+{
+    using System;
+
+    class StringConcatenation
+    {
+        static void Main()
+        {
+            string word1 = "Hello";
+            string word2 = "World";
+            object concatenated = word1 + " " + word2;
+            string sentence = (string)concatenated;
+
+            Console.WriteLine(sentence);
+        }
+    }
+}
