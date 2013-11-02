f5ff88f7b4d931ac279f29a3fc38047d1de44363
David Vesely <davidvesely@gmail.com>
2013-11-02 21:15:14 +0200

    Homeworks

---
+++ CSharp Part1/03. OperatorsExpressionsAndStatements/02.DivideBy5and7/DivideBy5and7.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Write a boolean expression that checks for given
+ * integer if it can be divided (without remainder)
+ * by 7 and 5 in the same time.
+ */
+
+namespace OperatorsExpressionsAndStatements
+{
+    using System;
+
+    class DivideBy5And7
+    {
+        static void Main()
+        {
+            int number = 4375;
+            if ((number % 5 == 0) && (number % 7 == 0))
+            {
+                Console.WriteLine("{0} can be divided by " +
+                    "7 and 5 in the same time", number);
+            }
+            else
+            {
+                Console.WriteLine("{0} can not be divided by " +
+                    "7 and 5 in the same time", number);
+            }
+        }
+    }
+}
---
+++ CSharp Part1/03. OperatorsExpressionsAndStatements/03.RectanglesArea/RectanglesArea.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Write an expression that calculates rectangle’s area by given width and height
+ */
+
+namespace OperatorsExpressionsAndStatements
+{
+    using System;
+
+    class RectanglesArea
+    {
+        static void Main()
+        {
+            int width = 140, height = 234;
+            int area = width * height;
+            Console.WriteLine("Rectangle's area is {0}", area);
+        }
+    }
+}
