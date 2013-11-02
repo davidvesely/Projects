37c8337f041d2b821d2ebe22efe83ed2776b8ead
David Vesely <davidvesely@gmail.com>
2013-11-02 20:58:23 +0200

    Homework

---
+++ CSharp Part1/03. OperatorsExpressionsAndStatements/01.OddOrEven/OddOrEven.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Write an expression that checks if given integer is odd or even
+ */
+
+namespace OperatorsExpressionsAndStatements
+{
+    using System;
+
+    class OddOrEven
+    {
+        static void Main()
+        {
+            int number = 123;
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Number {0} is even", number);
+            }
+            else
+            {
+                Console.WriteLine("Number {0} is odd", number);
+            }
+        }
+    }
+}
