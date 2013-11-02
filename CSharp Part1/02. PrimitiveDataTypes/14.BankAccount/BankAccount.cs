d1545fa14873a073ab0e459eb5157ed02645fc07
David Vesely <davidvesely@gmail.com>
2013-11-02 09:02:23 +0200

    Homework

--- CSharp Part1/02. PrimitiveDataTypes/14.BankAccount/BankAccount.cs
+++ CSharp Part1/02. PrimitiveDataTypes/14.BankAccount/BankAccount.cs
@@ -1,8 +1,9 @@
 ﻿/* 
- * Create a program that assigns null values to an integer
- * and to double variables. Try to print them on the console,
- * try to add some values or the null literal to them and see
- * the result.
+ * A bank account has a holder name (first name, middle name and
+ * last name), available amount of money (balance), bank name, IBAN,
+ * BIC code and 3 credit card numbers associated with the account.
+ * Declare the variables needed to keep the information for a single
+ * bank account using the appropriate data types and descriptive names.
  */
 namespace PrimitiveDataTypes
 {
@@ -12,7 +13,18 @@ namespace PrimitiveDataTypes
     {
         private static void Main()
         {
-            
+            string holderFirstName = "Pesho";
+            string holderMiddleName = "Ivanov";
+            string holderLastName = "Goshev";
+            decimal ballance = 12000;
+            string bankName = "Fibank";
+            string IBAN = null;
+            string BIC = null;
+            long creditCard1 = 1234567887654321;
+            long creditCard2 = 1111222233334444;
+            long creditCard3 = 5555666677778888;
+
+            Console.WriteLine("{0} {1} {2}", creditCard1, creditCard2, creditCard3);
         }
     }
 }
