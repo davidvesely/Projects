9db7d2d6375067a6cdfd6dd059698a75b0eb90d9
David Vesely <davidvesely@gmail.com>
2013-11-11 22:58:56 +0200

    practice

---
+++ CSharp Part1/Exam practice/DrunkenNumbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DrunkenNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = Math.Abs(int.Parse(Console.ReadLine()));
+            int mitkoBeers = 0, vaskoBeers = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string drunkNumber = Console.ReadLine().TrimStart('0', '-');
+                for (int j = 0; j < drunkNumber.Length; j++)
+                {
+                    if (j < drunkNumber.Length / 2)
+                    {
+                        mitkoBeers += drunkNumber[j] - '0';
+                    }
+                    else
+                    {
+                        vaskoBeers += drunkNumber[j] - '0';
+                    }
+                }
+
+                if (drunkNumber.Length % 2 == 1)
+                {
+                    int middleDigit = int.Parse(drunkNumber[drunkNumber.Length / 2].ToString());
+                    mitkoBeers += middleDigit;
+                }
+            }
+
+            // Determine the winner
+            if (mitkoBeers > vaskoBeers)
+            {
+                Console.WriteLine("M {0}", (mitkoBeers - vaskoBeers));
+            }
+            else if (mitkoBeers < vaskoBeers)
+            {
+                Console.WriteLine("V {0}", (vaskoBeers - mitkoBeers));
+            }
+            else
+            {
+                Console.WriteLine("No {0}", mitkoBeers + vaskoBeers);
+            }
+        }
+    }
+}
