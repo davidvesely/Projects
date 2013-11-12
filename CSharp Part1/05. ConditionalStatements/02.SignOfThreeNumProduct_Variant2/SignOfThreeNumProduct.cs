ff973d88eb8941ec40d07ccf5bd7acc201a57858
David Vesely <davidvesely@gmail.com>
2013-11-12 21:03:50 +0200

    Homework

---
+++ CSharp Part1/05. ConditionalStatements/02.SignOfThreeNumProduct_Variant2/SignOfThreeNumProduct.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Write a program that shows the sign (+ or -) of the
+ * product of three real numbers without calculating it.
+ * Use a sequence of if statements
+ * 
+ * Variant with only ordinary if statements
+ */
+
+using System;
+
+class SignOfThreeNumProduct
+{
+    static void Main()
+    {
+        Console.Write("Enter variable 1: ");
+        int variable1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter variable 2: ");
+        int variable2 = int.Parse(Console.ReadLine());
+        Console.Write("Enter variable 3: ");
+        int variable3 = int.Parse(Console.ReadLine());
+        bool isPositive = false;
+
+        if (variable1 < 0)
+        {
+            if (variable2 < 0)
+            {
+                if (true)
+                {
+                    
+                }
+            }
+        }
+    }
+}
