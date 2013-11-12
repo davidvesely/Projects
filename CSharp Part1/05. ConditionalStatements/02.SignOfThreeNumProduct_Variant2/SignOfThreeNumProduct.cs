26acac67dd7af6a0c5fe3d9a7901b6e39ab2d265
David Vesely <davidvesely@gmail.com>
2013-11-12 21:13:55 +0200

    minuses

--- CSharp Part1/05. ConditionalStatements/02.SignOfThreeNumProduct_Variant2/SignOfThreeNumProduct.cs
+++ CSharp Part1/05. ConditionalStatements/02.SignOfThreeNumProduct_Variant2/SignOfThreeNumProduct.cs
@@ -18,17 +18,30 @@ class SignOfThreeNumProduct
         int variable2 = int.Parse(Console.ReadLine());
         Console.Write("Enter variable 3: ");
         int variable3 = int.Parse(Console.ReadLine());
-        bool isPositive = false;
+        int minusCount = 0;
 
         if (variable1 < 0)
         {
-            if (variable2 < 0)
-            {
-                if (true)
-                {
-                    
-                }
-            }
+            minusCount++;
+        }
+
+        if (variable2 < 0)
+        {
+            minusCount++;
+        }
+
+        if (variable3 < 0)
+        {
+            minusCount++;
+        }
+
+        if (minusCount == 0 || minusCount == 2)
+        {
+            Console.WriteLine("The product will be positive.");
+        }
+        else
+        {
+            Console.WriteLine("The product will be negative.");
         }
     }
 }
