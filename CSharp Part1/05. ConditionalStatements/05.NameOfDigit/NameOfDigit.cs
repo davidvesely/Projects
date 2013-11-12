d7f49de1c7d39ce321f4d9578a99624e33185c7c
David Vesely <davidvesely@gmail.com>
2013-11-12 22:09:17 +0200

    Homeworks

---
+++ CSharp Part1/05. ConditionalStatements/05.NameOfDigit/NameOfDigit.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Write program that asks for a digit and depending on the
+ * input shows the name of that digit (in English) using a switch statement
+ */
+
+using System;
+
+class NameOfDigit
+{
+    static void Main()
+    {
+        string digit;
+        do
+        {
+            Console.Write("Please enter a digit [0..9]: ");
+            digit = Console.ReadLine();
+        } while ((digit.Length != 1) || (digit[0] < '0') || (digit[0] > '9'));
+
+        switch (digit[0])
+        {
+            case '0':
+                Console.WriteLine("Zero");
+                break;
+            case '1':
+                Console.WriteLine("One");
+                break;
+            case '2':
+                Console.WriteLine("Two");
+                break;
+            case '3':
+                Console.WriteLine("Three");
+                break;
+            case '4':
+                Console.WriteLine("Four");
+                break;
+            case '5':
+                Console.WriteLine("Five");
+                break;
+            case '6':
+                Console.WriteLine("Six");
+                break;
+            case '7':
+                Console.WriteLine("Seven");
+                break;
+            case '8':
+                Console.WriteLine("Eight");
+                break;
+            case '9':
+                Console.WriteLine("Nine");
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ CSharp Part1/05. ConditionalStatements/06.QuadraticEquationSolver/QuadraticEquationSolver.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Write a program that enters the coefficients a, b and c of a quadratic equation
+ *       a*x2 + b*x + c = 0
+ * and calculates and prints its real roots. 
+ * Note that quadratic equations may have 0, 1 or 2 real roots
+ */
+
+using System;
+
+class QuadraticEquationSolver
+{
+    static void Main()
+    {
+        Console.WriteLine("Please provide the coefficients of the\n" + 
+            "quadratic equation a*x2 + b*x + c = 0");
+        Console.Write("a: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("b: ");
+        int b = int.Parse(Console.ReadLine());
+        Console.Write("c: ");
+        int c = int.Parse(Console.ReadLine());
+
+        double discriminant = (b * b) - (4 * a * c);
+
+        if (discriminant < 0)
+        {
+            Console.WriteLine("There are no real roots.");
+        }
+        else if (discriminant == 0)
+        {
+            double root = -b / (2 * a);
+            Console.WriteLine("The only root of the equation is {0:F4}", root);
+        }
+        else
+        {
+            double root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+            Console.WriteLine("Roots of the equation are: {0:F4} and {1:F4}", root1, root2);
+        }
+    }
+}
