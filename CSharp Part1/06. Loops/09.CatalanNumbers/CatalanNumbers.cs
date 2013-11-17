afb524edaabe07a64e2ac5f1d4ca3f92975836c5
David Vesely <davidvesely@gmail.com>
2013-11-17 15:53:24 +0200

    Homework

---
+++ CSharp Part1/06. Loops/09.CatalanNumbers/CatalanNumbers.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * In the combinatorial mathematics, the Catalan numbers are
+ * calculated by the following formula
+ * (2*n)! / (n + 1)! * n!
+ */
+
+using System;
+
+class CatalanNumbers
+{
+    static void Main()
+    {
+    }
+}
