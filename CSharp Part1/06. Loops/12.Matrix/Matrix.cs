ea4eef77aa3e0a6edbeaa97a8a768e927eebb220
David Vesely <davidvesely@gmail.com>
2013-11-17 22:18:26 +0200

    set

---
+++ CSharp Part1/06. Loops/12.Matrix/Matrix.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Write a program that reads from the console a positive
+ * integer number N (N < 20) and outputs a matrix
+ */
+
+using System;
+
+class Matrix
+{
+    static void Main()
+    {
+        int size;
+        Console.Write("Provide size of the matrix: ");
+        if (!int.TryParse(Console.ReadLine(), out size) || (size >= 20) || (size < 0))
+        {
+            Console.WriteLine("Bad input");
+            return;
+        }
+
+        for (int i = 1; i <= size; i++) // rows
+        {
+            for (int j = 0; j < size; j++) // cols
+            {
+                Console.Write("{0,3}", i + j);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CSharp Part1/06. Loops/13.TrailingZerosFactorial/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Write a program that calculates for given N
+ * how many trailing zeros present at the end of the number N!
+ */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n;
+        Console.Write("Please enter N: ");
+        bool isValid = int.TryParse(Console.ReadLine(), out n);
+        if (!isValid && (n < 0))
+        {
+            Console.WriteLine("Please enter valid number!");
+            return;
+        }
+
+        int zeroCounter = 0;
+        while (true)
+        {
+            
+        }
+    }
+}
