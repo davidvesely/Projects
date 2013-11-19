b78e27b3acc534d0ea9b06b8052583a61b38ad4c
David Vesely <davidvesely@gmail.com>
2013-11-19 21:34:36 +0200

    Homework

--- CSharp Part1/06. Loops/14.SpiralMatrix/SpiralMatrix.cs
+++ CSharp Part1/06. Loops/14.SpiralMatrix/SpiralMatrix.cs
@@ -12,7 +12,7 @@ class Program
         Console.Write("N = ");
         int n = int.Parse(Console.ReadLine());
 
-        int[,] spiralMa3x = new int[n, n];
+        int[,] spiralMa3X = new int[n, n];
         int pos = 0;
         int count = n;
         int value = -n;
@@ -24,24 +24,24 @@ class Program
             for (int i = 0; i < count; i++)
             {
                 sum += value;
-                spiralMa3x[sum / n, sum % n] = pos++;
+                spiralMa3X[sum / n, sum % n] = pos++;
             }
             value *= n;
             count--;
             for (int i = 0; i < count; i++)
             {
                 sum += value;
-                spiralMa3x[sum / n, sum % n] = pos++;
+                spiralMa3X[sum / n, sum % n] = pos++;
             }
         } while (count > 0);
 
         // Print the generated matrix
-        int p = (spiralMa3x.GetLength(0) * spiralMa3x.GetLength(1) - 1).ToString().Length + 1;
-        for (int i = 0; i < spiralMa3x.GetLength(0); i++)
+        int p = (spiralMa3X.GetLength(0) * spiralMa3X.GetLength(1) - 1).ToString().Length + 1;
+        for (int i = 0; i < spiralMa3X.GetLength(0); i++)
         {
-            for (int j = 0; j < spiralMa3x.GetLength(1); j++)
+            for (int j = 0; j < spiralMa3X.GetLength(1); j++)
             {
-                Console.Write(spiralMa3x[i, j].ToString().PadLeft(p, ' '));
+                Console.Write(spiralMa3X[i, j].ToString().PadLeft(p, ' '));
             }
             Console.WriteLine();
         }
