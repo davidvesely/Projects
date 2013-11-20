d37b1c350b401caf6dc26564dfebf83c5b2271d9
David Vesely <davidvesely@gmail.com>
2013-11-20 21:56:55 +0200

    Secrets of numbers

---
+++ CSharp Part1/Exam practice/2. SecretNumbers/SecretNumbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SecretNumbers
+{
+    static void Main()
+    {
+        string inputNumber = Console.ReadLine().TrimStart('-');
+        // Calc the special sum
+        long specialSum = 0;
+        int digitPosCounter = 1;
+        for (int i = inputNumber.Length - 1; i >= 0 ; i--)
+        {
+            int digit = inputNumber[i] - '0';
+            if (digitPosCounter % 2 == 0) // Even
+            {
+                specialSum += digit * digit * digitPosCounter;
+            }
+            else
+            {
+                specialSum += digit * digitPosCounter * digitPosCounter;
+            }
+            digitPosCounter++;
+        }
+        Console.WriteLine(specialSum);
+
+        // Find the sequence
+        int secretSeqLen = (int)specialSum % 10;
+        if (secretSeqLen == 0)
+        {
+            Console.WriteLine("{0} has no secret alpha-sequence", inputNumber);
+        }
+        else
+        {
+            int sequenceCounter = (int)specialSum % 26;
+            char[] sequence = new char[secretSeqLen];
+            for (int i = 0; i < secretSeqLen; i++)
+            {
+                sequence[i] = (char)('A' + sequenceCounter);
+                sequenceCounter++;
+                if (sequenceCounter == 26)
+                {
+                    sequenceCounter = 0;
+                }
+            }
+            Console.WriteLine(sequence);
+        }
+    }
+}
