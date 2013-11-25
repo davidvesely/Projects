0f3416341ea666372e522a08b9b3a97403c2e218
davidvesely <davidvesely@gmail.com>
2013-11-25 23:11:06 +0200

    Exam practice

---
+++ CSharp Part1/Exam Practice 2/2. Least Majority Multiple/LeastMajorityMultiple.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LeastMajorityMultiple
+{
+    static void Main()
+    {
+        int[] numbers = new int[5];
+        for (int i = 0; i < 5; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        int countLeastMajority = 0;
+        int currentNumber = 0;
+        while (countLeastMajority < 3)
+        {
+            countLeastMajority = 0;
+            currentNumber++;
+            for (int i = 0; i < 5; i++)
+            {
+                if (currentNumber % numbers[i] == 0)
+                {
+                    countLeastMajority++;
+                    if (countLeastMajority == 3)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(currentNumber);
+    }
+}
