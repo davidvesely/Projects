a44ebbaa1718b9bbb7dfdbdae755338bd4145f0e
davidvesely <davidvesely@gmail.com>
2013-11-25 21:44:09 +0200

    Exam practice

--- CSharp Part1/Exam practice/4. Binary Digits Count/BinaryDigitsCount.cs
+++ CSharp Part1/Exam practice/4. Binary Digits Count/BinaryDigitsCount.cs
@@ -6,5 +6,25 @@ class BinaryDigitsCount
 {
     static void Main()
     {
+        char binaryDigit = Console.ReadLine()[0];
+        int numberCount = int.Parse(Console.ReadLine());
+        int[] counter = new int[numberCount];
+        for (int i = 0; i < numberCount; i++)
+        {
+            uint number = uint.Parse(Console.ReadLine());
+            string binary = Convert.ToString(number, 2);
+            for (int j = 0; j < binary.Length; j++)
+            {
+                if (binaryDigit == binary[j])
+                {
+                    counter[i]++;
+                }
+            }
+        }
+
+        foreach (int count in counter)
+        {
+            Console.WriteLine(count);
+        }
     }
 }
