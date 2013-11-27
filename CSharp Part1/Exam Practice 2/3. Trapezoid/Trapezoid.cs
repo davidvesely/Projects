3bd4882da810ab2bd665834d192d691eaed5d618
davidvesely <davidvesely@gmail.com>
2013-11-27 21:02:24 +0200

    Exam practice

---
+++ CSharp Part1/Exam Practice 2/3. Trapezoid/Trapezoid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Trapezoid
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine(new string('.', number) + new string('*', number));
+        for (int i = 1; i < number; i++)
+        {
+            Console.Write(new string('.', number - i));
+            Console.Write("*");
+            Console.Write(new string('.', (i - 1) + (number - 1)));
+            Console.WriteLine("*");
+        }
+        Console.WriteLine(new string('*', number * 2));
+    }
+}
