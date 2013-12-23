483da671f76bc296776e242addc06f731f2dd749
David Vesely <davidvesely@gmail.com>
2013-12-23 12:41:40 +0200

    aspx

---
+++ ASP.NET Webforms/01.WebForms Intro/01.SimpleASPX/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _01.SimpleASPX
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonShowGreeting_OnClick(object sender, EventArgs e)
+        {
+            LabelGreeting.Text = "Hello " + TextBoxName.Text;
+            LabelAssembly.Text = Assembly.GetExecutingAssembly().Location;
+        }
+    }
+}
