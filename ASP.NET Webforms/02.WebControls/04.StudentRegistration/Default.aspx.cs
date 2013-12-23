4eb90db16276921bd4a620b3cbf5a455477c1cfd
David Vesely <davidvesely@gmail.com>
2013-12-23 17:41:51 +0200

    aspx

---
+++ ASP.NET Webforms/02.WebControls/04.StudentRegistration/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace _04.StudentRegistration
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected readonly StringBuilder _specialities = new StringBuilder();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int[] indices = ListBoxSpeciality.GetSelectedIndices();
+            foreach (int index in indices)
+            {
+                _specialities.Append(ListBoxSpeciality.Items[index]);
+                if (index != indices.Length - 1)
+                {
+                    _specialities.Append(", ");
+                }
+            }
+        }
+    }
+}
