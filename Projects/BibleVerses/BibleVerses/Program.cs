a74613abe4f18155a74a249ebf388d45d4123ca7
David Vesely <davidvesely@gmail.com>
2014-06-06 23:50:07 +0300

    commit

--- Projects/BibleVerses/BibleVerses/Program.cs
+++ Projects/BibleVerses/BibleVerses/Program.cs
@@ -1,23 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using HtmlAgilityPack;
-using Microsoft.Office.Interop.Word;
-using WordApplication = Microsoft.Office.Interop.Word.Application;
 using BibleVerses.Models;
+using System.IO;
 
 namespace BibleVerses
 {
     public class VerseReader
     {
         private static StreamWriter fileOutput;
-        private static int startTextIndex = 1;
-        private static readonly bool ShouldAppend = false;
+
         // 0 - Book; 1 - Chapter; 2 - start verse; 3 - end verse
         // Veren
         //private const string READER_URL = @"http://bible.netbg.com/bible/paralel/bible.php?b={0}&c={1}&r=&v1={2}&v2={3}&vt=1&c7=1&st=&sa=1&rt=2&l=1&cm=2";
@@ -171,92 +167,15 @@ namespace BibleVerses
             };
         }
 
-        private static void CreateDoc(List<BibleVerse> verseList)
-        {
-#if DEBUG
-            string fileName = "D:\\test.docx";
-            Console.WriteLine("Filename is {0}\n", fileName);
-#else
-            Console.Write("\nPlease enter the destination docx file: ");
-            string fileName = Console.ReadLine();
-#endif
-            bool isNewFile = true;
-            WordApplication application = new WordApplication();
-            // Delete the file before writing
-            if (!ShouldAppend)
-                File.Delete(fileName);
-
-            if (File.Exists(fileName))
-            {
-                application.Documents.Open(fileName);
-                isNewFile = false;
-            }
-            else
-            {
-                application.Documents.Add();
-            }
-
-            object pageBreak = WdBreakType.wdPageBreak;
-            Paragraph paragraph;
-            for (int i = 0; i < verseList.Count; i++)
-			{
-                // The number of text
-                paragraph = application.ActiveDocument.Content.Paragraphs.Add();
-                paragraph.Range.Text = (i + startTextIndex).ToString();
-                paragraph.set_Style(application.ActiveDocument.Styles["Heading 1"]);
-                paragraph.Range.InsertParagraphAfter();
-                // Verse
-                paragraph = application.ActiveDocument.Content.Paragraphs.Add();
-                paragraph.Range.Text = verseList[i].Text;
-                paragraph.Range.InsertParagraphAfter();
-                // Place
-                paragraph = application.ActiveDocument.Content.Paragraphs.Add();
-                paragraph.Range.Text = verseList[i].BiblePlace.FullLocation;
-                paragraph.set_Style(application.ActiveDocument.Styles["Heading 2"]);
-                paragraph.Range.InsertParagraphAfter();
-                // Page Break
-                if (i < verseList.Count - 1)
-                    paragraph.Range.InsertBreak(ref pageBreak);
-                System.Runtime.InteropServices.Marshal.ReleaseComObject(paragraph);
-
-                // Check for overlapping
-                BiblePlace currentPlace = verseList[i].BiblePlace;
-                var q = verseList.GetRange(0, i).Where(a => a.BiblePlace.IsOverlaping(currentPlace));
-                if (q.Any())
-                {
-                    string output = string.Format("Row {0}: Place {1} is overlapping the following places: {2}\n",
-                        i + 1, currentPlace.ToString(), string.Join("; ", q.Select(a => a.BiblePlace)));
-                    Console.WriteLine(output);
-                    fileOutput.WriteLine(output);
-                }
-            }
-
-            try
-            {
-                if (isNewFile)
-                    application.ActiveDocument.SaveAs(fileName);
-                else
-                    application.ActiveDocument.Save();
-                application.ActiveDocument.Close();
-                application.Quit();
-                System.Runtime.InteropServices.Marshal.ReleaseComObject(application);
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
-            }
-            catch
-            {
-                paragraph = null;
-                application = null;
-            }
-        }
-
         static void Main(string[] args)
         {
-            using (fileOutput = new StreamWriter("output.txt", false, Encoding.UTF8))
-            {
-                List<BibleVerse> texts = GetAllTexts();
-                CreateDoc(texts);
-            }
+            //using (fileOutput = new StreamWriter("output.txt", false, Encoding.UTF8))
+            //{
+            //    List<BibleVerse> texts = GetAllTexts();
+            //    WordHandling.CreateDoc(texts, fileOutput);
+            //}
+
+            WordHandling.InsertPictures();
         }
     }
 }
