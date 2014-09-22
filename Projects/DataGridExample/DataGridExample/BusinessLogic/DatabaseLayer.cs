3696401395c50d100a7001169587108e93ef3efe
David Vesely <davidvesely@gmail.com>
2014-09-22 20:50:26 +0300

    last changes

--- Projects/DataGridExample/DataGridExample/BusinessLogic/DatabaseLayer.cs
+++ Projects/DataGridExample/DataGridExample/BusinessLogic/DatabaseLayer.cs
@@ -23,6 +23,7 @@ namespace DataGridExample.BusinessLogic
             db.SaveChanges();
         }
 
+        // Заявка за подаване на DataSource на грид
         public BindingList<OPR> GetOPRRows()
         {
             //var objects = db.OPR.ToList();
@@ -36,21 +37,24 @@ namespace DataGridExample.BusinessLogic
 
         public BindingList<OPRITEM> GetOPRITEMRows(int id)
         {
+            // LINQ заявка, за взимането на всички редове с ID = id
             var query = from it in db.OPRITEM
                         where it.ID == id
                         select it;
+            // Данните се зареждат в паметта
             query.Load();
+            // За да може грида да следи промяната на данните, и след това да ги записва, query-то се превръща в BindingList
             return query.ToBindingList();
         }
 
         public void InsertOPRITEM(OPRITEM item)
         {
             db.OPRITEM.Add(item);
-
         }
 
         public List<string> GetDocType()
         {
+            // Взима всички уникални (distinct) стойности в колоната TYPE_DOC
             List<string> types = (from t in db.OPR
                                   select t.TYPE_DOC).Distinct().ToList();
             return types;
@@ -58,9 +62,13 @@ namespace DataGridExample.BusinessLogic
 
         public int GetNextPos(int id)
         {
-            return ((from oi in db.OPRITEM
-                    where oi.ID == id
-                    select (short?)oi.POS).Max() ?? 0) + 1;
+            // LINQ заявка за взимане на MAX стойност на POS със съответното ID
+            // select (short?)oi.POS -> това се прави защото ако select-а не върне данни, ще върне null
+            // short? е nullable
+            var maxId = (from oi in db.OPRITEM
+                         where oi.ID == id
+                         select (short?)oi.POS).Max();
+            return (maxId ?? 0) + 1;
         }
 
         public int GetNextID()
